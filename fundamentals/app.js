312b49d3a41040e6bbcd53cb395391df3cc64d6e
Kumar Abhijeet <ab.progr@gmail.com>
2020-05-19 17:09:27 +0530

    using console methods

---
+++ fundamentals/app.js
@@ -0,0 +1,23 @@
+// console methods
+
+console.clear();
+
+console.log("Hello Vanilla JS");
+console.log(123);
+console.log(true);
+console.log([1, 2, 3, 4, 5]);
+console.log({ a: 1, b: 2, c: 3 });
+
+console.table({ a: 1, b: 2, c: 3 });
+console.error("This is how you log errors to console");
+console.warn("This is a warning on console");
+
+console.time("test");
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.timeEnd("test");
+
+// end of console methods
