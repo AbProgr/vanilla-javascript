451a58c28659e34cf600f99eda298cbf0655c32e
Kumar Abhijeet <ab.progr@gmail.com>
2020-05-19 17:50:49 +0530

    using variables

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -1,5 +1,4 @@
 // console methods
-
 console.clear();
 
 console.log("Hello Vanilla JS");
@@ -19,5 +18,38 @@ console.log(3);
 console.log(4);
 console.log(5);
 console.timeEnd("test");
-
 // end of console methods
+
+// variables
+// var
+var name = "vanilla";
+console.log(name);
+name = "javascript";
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting = "Hello JS";
+console.log(greeting);
+// end of var
+
+// let
+let test = "let";
+console.log(test);
+test = "variable";
+console.log(test);
+// end of let
+
+// const
+const test1 = "const";
+console.log(test1);
+test1 = true; // will throw - TypeError: Assignment to constant variable
+
+const test2 = { name: "const" };
+console.log(test2);
+test2.name = "modified value";
+console.log(test2); // no error - as object/arrays are ref type and point to a memory location only
+test2 = [1, 2, 3];
+console.log(test2); // error - TypeError: Assignment to constant variable. - as we cannot assign new value
+// end of const
+// end of variable
